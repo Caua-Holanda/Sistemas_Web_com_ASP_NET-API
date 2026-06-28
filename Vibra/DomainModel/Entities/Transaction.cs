170449a925a67924b39b4ed2aa327cdcfba6c98e
Cauã Holanda <cauaholanda@hotmail.com>
2026-06-28 18:54:42 -0300

    fix: ajuste transaction denialReason

--- Vibra/DomainModel/Entities/Transaction.cs
+++ Vibra/DomainModel/Entities/Transaction.cs
@@ -10,7 +10,7 @@ namespace Vibra.DomainModel.Entities
         public decimal Amount { get; set; }
         public DateTime Timestamp { get; set; }
         public TransactionStatus Status { get; set; }
-        public string DenialReason { get; set; }
+        public string DenialReason { get; set; } = string.Empty;
         public DateTime? LastAuthorization { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
